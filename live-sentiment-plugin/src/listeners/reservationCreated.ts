c8670a1df9873cde1d77fbf89556c77b69a2af13
slintab <slintab@icloud.com>
2025-11-16 21:07:44 +0000

    updated ts types

--- live-sentiment-plugin/src/listeners/reservationCreated.ts
+++ live-sentiment-plugin/src/listeners/reservationCreated.ts
@@ -1,16 +1,6 @@
-import { Actions, ITask, IWorker } from "@twilio/flex-ui";
-import { Call } from "@twilio/flex-ui/src/task_channels";
-import { EventEmitter } from "events";
 import LiveTranscriptionService from "../services/LiveTranscriptionService";
 
-//import SuggestionService from "../services/SuggestionService";
-
-interface Reservation extends EventEmitter {
-  task: ITask;
-  on(event: "accepted", listener: (reservation: Reservation) => void): this;
-}
-
-export default (reservation: Reservation) => {
+export default (reservation: TaskRouter.Reservation) => {
   reservation.on("accepted", async (reservation) => {
     const { taskChannelUniqueName, attributes, sid } = reservation.task;
 
